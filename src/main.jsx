86694a06333704b56c76d12443c9b1e4734e35b2
Nannita1403 <nannimagi@gmail.com>
2024-10-25 16:14:16 +0200

    update modules

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,18 @@
-import { createRoot } from 'react-dom/client'
+import  { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react';
 import ThemePageProvider from './providers/ThemePageProvider.jsx';
+import { ChakraProvider } from '@chakra-ui/react';
+import React from 'react';
 
-createRoot(document.getElementById('root')).render(
-<BrowserRouter>
+createRoot(document.getElementById('root')).render( 
   <ChakraProvider>
-    <ThemePageProvider>
+<BrowserRouter>
+<ThemePageProvider>
         <App/>
     </ThemePageProvider>
-  </ChakraProvider>
-  </BrowserRouter>
+</BrowserRouter>
+</ChakraProvider>
 
 );
